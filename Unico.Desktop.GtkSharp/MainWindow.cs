01004eec06964f0475ec66c974e6c4540bbaf0ea
alexlee <lu.lee05@gmail.com>
2015-03-26 20:01:00 +0800

    add gtk client

---
+++ Unico.Desktop.GtkSharp/MainWindow.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Gtk;
+using Xilium.CefGlue;
+using Unico.Desktop;
+using System.Runtime.InteropServices;
+
+public partial class MainWindow: Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        DefaultWidth = 800;
+        DefaultHeight = 600;
+        Show ();
+        DeleteEvent += (sender, e) =>
+        {        
+            if (CefRuntime.Platform == CefRuntimePlatform.Windows)
+                Application.Quit();
+            else 
+                CefRuntime.QuitMessageLoop();
+            e.RetVal = true;
+        };
+    }
+
+    protected override void OnRealized()
+    {
+        base.OnRealized();
+        ChildVisible = true;
+        var windowInfo = CefWindowInfo.Create();
+        switch (CefRuntime.Platform)
+        {
+            case CefRuntimePlatform.Windows:
+                var parentHandle = gdk_win32_drawable_get_handle(GdkWindow.Handle);
+                windowInfo.SetAsChild(parentHandle, new CefRectangle(0, 0, 0, 0)); // TODO: set correct  x, y, width, height  to do not waiting OnSizeAllocated event
+                break;
+
+            case CefRuntimePlatform.Linux:
+                Console.WriteLine("REALIZED - RAW = {0}, HANDLE = {1}", Raw, Handle);
+                windowInfo.SetAsChild(Handle, new CefRectangle(0, 0, 0, 0));
+                break;
+
+            case CefRuntimePlatform.MacOSX:
+            default:
+                throw new NotSupportedException();
+        }
+//        windowInfo.SetAsChild(Handle, new CefRectangle(0, 0, 0, 0));
+        CefBrowserHost.CreateBrowser(windowInfo, new SimpleCefClient(), new CefBrowserSettings(), "www.baidu.com");
+
+     //   _core.Create(windowInfo);
+    }
+    [DllImport("libgdk-win32-2.0-0.dll", CallingConvention = CallingConvention.Cdecl)]
+    internal static extern IntPtr gdk_win32_drawable_get_handle(IntPtr raw);
+}
---
+++ Unico.Desktop.GtkSharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Gtk;
+using System.IO;
+using Xilium.CefGlue;
+using System.Reflection;
+
+namespace Unico.Desktop.GtkSharp
+{
+    class MainClass
+    {
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            CefRuntime.Load();
+            var mainArgs = new CefMainArgs(args);
+            var app = new SimpleCefApp();
+            var exitCode = CefRuntime.ExecuteProcess(mainArgs, app, IntPtr.Zero);
+            if (exitCode != -1)
+            {
+                Console.WriteLine("CefRuntime.ExecuteProcess() returns {0}", exitCode);
+                return exitCode;
+            }
+
+            var libPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            var settings = new CefSettings
+            { 
+                SingleProcess = false,
+                MultiThreadedMessageLoop = CefRuntime.Platform == CefRuntimePlatform.Windows,
+                ResourcesDirPath =libPath,
+                LocalesDirPath = Path.Combine(libPath , "locales"),
+                NoSandbox = CefRuntime.Platform == CefRuntimePlatform.Linux,
+                WindowlessRenderingEnabled = true
+            };
+            CefRuntime.Initialize(mainArgs, settings, app, IntPtr.Zero);
+            CefRuntime.RegisterSchemeHandlerFactory("http", "server.unico.local", new SimpleSchemeHandlerFactory());
+
+            Application.Init();
+            var win = new MainWindow();
+            win.Show();
+
+            if (CefRuntime.Platform == CefRuntimePlatform.Windows) 
+                Application.Run();
+            else
+                CefRuntime.RunMessageLoop();
+
+            Application.Run();
+            return 0;
+        }
+    }
+}
