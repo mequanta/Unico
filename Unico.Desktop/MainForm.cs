9ed783607820215abc0b1efbdaa898e8254e5cab
alex <alex@localhost.localdomain>
2015-03-27 14:48:26 +0800

    a

--- Unico.Desktop/MainForm.cs
+++ Unico.Desktop/MainForm.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
-using System.Threading;
 using System.Drawing;
-using System.ComponentModel;
 using Xilium.CefGlue;
 
 namespace Unico.Desktop
@@ -14,9 +12,10 @@ namespace Unico.Desktop
             Size = new Size(800, 600);
             SetStyle(ControlStyles.UserPaint, true);
         }
-
+            
         protected override void OnHandleCreated(EventArgs e)
         {
+            base.OnHandleCreated(e);
             var winInfo = CefWindowInfo.Create();
             winInfo.SetAsChild(Handle, new CefRectangle { X = 0, Y = 0, Width = Width, Height = Height });
             CefBrowserHost.CreateBrowser(winInfo, new SimpleCefClient(), new CefBrowserSettings(), "www.baidu.com");
